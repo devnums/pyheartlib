406b409f51e62881c8dc21eab9b8104c5e513126
sadeghmdi <sadegh0@gmail.com>
2023-05-02 00:41:33 +0330

    add dataset preparation

---
+++ model/data_preparation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pyecg.io import load_data
+from pyecg.data_rpeak import RpeakData, ECGSequence
+
+
+train_set = [201, 203]
+test_set = [202, 210, 219, 221, 222]
+
+
+# create train dataset
+rpeak_data = RpeakData(base_path="./data", remove_bl=False, lowpass=False)
+annotated_records, samples_info = rpeak_data.save_samples(
+    rec_list=train_set, file_path="./data/train.rpeak", win_size=10 * 360, stride=360
+)
+annotated_records, samples_info = load_data("./data/train.rpeak")
+print("number of generated sampels:", str(len(samples_info)))
+
+
+# for verfication
+training_generator = ECGSequence(
+    annotated_records, samples_info, binary=True, batch_size=1, raw=True, interval=36
+)
+
+i = 0
+label = training_generator.__getitem__(i)[1]  # excerpt label
+seq = training_generator.__getitem__(i)[0]  # excerpt values
+
+print("len label:", len(label[0]), "len seq:", len(seq[0]))
