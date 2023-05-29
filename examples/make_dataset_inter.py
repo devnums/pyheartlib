b0a29043e7815bc35bef002c33592b014b8f4f98
sadeghmdi <sadegh0@gmail.com>
2023-05-29 12:33:46 +0330

    refactor: edit exampes

--- examples/make_dataset_inter.py
+++ examples/make_dataset_inter.py
@@ -8,7 +8,13 @@ from pyheartlib.beat_info import BeatInfo
 
 # Inter-patient
 # Lets create a BeatData object.the train set by creating
-beatdata = BeatData(base_path="./data", win=[200, 200], remove_bl=False, lowpass=False, progress_bar=True)
+beatdata = BeatData(
+    base_path="./data",
+    win=[200, 200],
+    remove_bl=False,
+    lowpass=False,
+    progress_bar=True,
+)
 
 # Create a BeatInfo object for features calculations.
 beatinfo = BeatInfo(beat_loc=beatdata.beat_loc)
@@ -17,25 +23,29 @@ beatinfo = BeatInfo(beat_loc=beatdata.beat_loc)
 def F_new_feature1(self):
     return 1.09
 
+
 def F_new_feature2(self):
     post = beatinfo.F_post_rri()
     pre = beatinfo.F_pre_rri()
     return post - pre
 
+
 new_features = [F_new_feature1, F_new_feature2]
-beatinfo.add_features(new_features)  
-print(beatinfo.available_features()) 
+beatinfo.add_features(new_features)
+print(beatinfo.available_features())
 
 
-beatinfo.select_features(["F_beat_max", "F_beat_min", "F_beat_skewness", "F_new_feature1", "F_new_feature2"])
+beatinfo.select_features(
+    ["F_beat_max", "F_beat_min", "F_beat_skewness", "F_new_feature1", "F_new_feature2"]
+)
 
 # use the save_dataset_inter method to create the dataset file.
 # The file will be saved in the base data directory.
-beatdata.save_dataset_inter(beatdata.config["DS1"][17:18], beatinfo, file="train.beat")
+beatdata.save_dataset_inter(["209"], beatinfo, file="train.beat")
 
 # In a similar way for validation and test sets
-beatdata.save_dataset_inter(beatdata.config["DS1"][18:20], beatinfo, file="val.beat")
-beatdata.save_dataset_inter(beatdata.config["DS2"][1:2], beatinfo, file="test.beat")
+beatdata.save_dataset_inter(["215", "220"], beatinfo, file="val.beat")
+beatdata.save_dataset_inter(["103"], beatinfo, file="test.beat")
 
 # Loading the sets
 train_ds = beatdata.load_data(file_name="train.beat")
@@ -47,4 +57,4 @@ stat_report = beatdata.report_stats_table(
     [train_ds["labels"], val_ds["labels"], test_ds["labels"]], ["Train", "Val", "Test"]
 )
 print(stat_report)
-print(train_ds['beat_feats'].tail(3))
+print(train_ds["beat_feats"].tail(3))
