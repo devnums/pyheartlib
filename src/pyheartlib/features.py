cead088d46f1e61fd82c4917af6fc2c3b9e166b6
sadeghmdi <sadegh0@gmail.com>
2023-12-01 13:07:26 +0330

    feat: add new args

--- src/pyheartlib/features.py
+++ src/pyheartlib/features.py
@@ -82,7 +82,7 @@ def get_stat_features(data, features="all"):
     return features_arr
 
 
-def get_hrv_features(rri, features="all"):
+def get_hrv_features(rri=None, features="all", only_names=False):
     """
     Computes hrv features for the input samples.
 
@@ -116,91 +116,97 @@ def get_hrv_features(rri, features="all"):
     else:
         flist = features  # features list
 
-    num_samples = rri.shape[0]
-    num_features = len(flist)
-    features_arr = np.ndarray((num_samples, num_features), dtype=np.float32)
+    if not only_names:
+        num_samples = rri.shape[0]
+        num_features = len(flist)
+        features_arr = np.ndarray(
+            (num_samples, num_features), dtype=np.float32
+        )
 
-    # successive RR interval differences
-    sd = np.diff(rri, axis=1)
+        # successive RR interval differences
+        sd = np.diff(rri, axis=1)
 
-    # calculate meanrr
-    if bool({"meanrr", "nsdrr", "nrmssd"} & set(flist)):
-        meanrr = np.mean(rri, axis=1)
+        # calculate meanrr
+        if bool({"meanrr", "nsdrr", "nrmssd"} & set(flist)):
+            meanrr = np.mean(rri, axis=1)
 
-    # calculate meanrr
-    if bool({"sdrr", "nsdrr"} & set(flist)):
-        sdrr = np.std(rri, axis=1)
+        # calculate meanrr
+        if bool({"sdrr", "nsdrr"} & set(flist)):
+            sdrr = np.std(rri, axis=1)
 
-    # calculate rmssd
-    if bool({"rmssd", "nrmssd"} & set(flist)):
-        rmssd = np.sqrt(np.mean(sd**2, axis=1))
+        # calculate rmssd
+        if bool({"rmssd", "nrmssd"} & set(flist)):
+            rmssd = np.sqrt(np.mean(sd**2, axis=1))
 
-    # calculate hr
-    if bool({"meanhr", "maxhr", "minhr", "medianhr", "sdhr"} & set(flist)):
-        hr = 60000 / rri
+        # calculate hr
+        if bool({"meanhr", "maxhr", "minhr", "medianhr", "sdhr"} & set(flist)):
+            hr = 60000 / rri
 
-    if "meanrr" in flist:
-        ix = flist.index("meanrr")
-        features_arr[:, ix] = meanrr
+        if "meanrr" in flist:
+            ix = flist.index("meanrr")
+            features_arr[:, ix] = meanrr
 
-    if "sdrr" in flist:
-        ix = flist.index("sdrr")
-        features_arr[:, ix] = sdrr
+        if "sdrr" in flist:
+            ix = flist.index("sdrr")
+            features_arr[:, ix] = sdrr
 
-    if "medianrr" in flist:
-        ix = flist.index("medianrr")
-        features_arr[:, ix] = np.ma.median(rri, axis=1)
+        if "medianrr" in flist:
+            ix = flist.index("medianrr")
+            features_arr[:, ix] = np.ma.median(rri, axis=1)
 
-    if "rangerr" in flist:
-        ix = flist.index("rangerr")
-        features_arr[:, ix] = np.amax(rri, axis=1) - np.amin(rri, axis=1)
+        if "rangerr" in flist:
+            ix = flist.index("rangerr")
+            features_arr[:, ix] = np.amax(rri, axis=1) - np.amin(rri, axis=1)
 
-    if "nsdrr" in flist:
-        ix = flist.index("nsdrr")
-        features_arr[:, ix] = sdrr / meanrr
+        if "nsdrr" in flist:
+            ix = flist.index("nsdrr")
+            features_arr[:, ix] = sdrr / meanrr
 
-    if "sdsd" in flist:
-        ix = flist.index("sdsd")
-        features_arr[:, ix] = np.std(sd, axis=1)
+        if "sdsd" in flist:
+            ix = flist.index("sdsd")
+            features_arr[:, ix] = np.std(sd, axis=1)
 
-    # Root mean square of successive RR interval differences
-    if "rmssd" in flist:
-        ix = flist.index("rmssd")
-        features_arr[:, ix] = rmssd
+        # Root mean square of successive RR interval differences
+        if "rmssd" in flist:
+            ix = flist.index("rmssd")
+            features_arr[:, ix] = rmssd
 
-    if "nrmssd" in flist:
-        ix = flist.index("nrmssd")
-        features_arr[:, ix] = rmssd / meanrr
+        if "nrmssd" in flist:
+            ix = flist.index("nrmssd")
+            features_arr[:, ix] = rmssd / meanrr
 
-    if "prr50" in flist:
-        ix = flist.index("prr50")
-        prr50 = 100 * np.sum(np.abs(sd) > 5, axis=1) / sd.shape[1]
-        features_arr[:, ix] = prr50
+        if "prr50" in flist:
+            ix = flist.index("prr50")
+            prr50 = 100 * np.sum(np.abs(sd) > 5, axis=1) / sd.shape[1]
+            features_arr[:, ix] = prr50
 
-    if "meanhr" in flist:
-        ix = flist.index("meanhr")
-        features_arr[:, ix] = np.mean(hr, axis=1)
+        if "meanhr" in flist:
+            ix = flist.index("meanhr")
+            features_arr[:, ix] = np.mean(hr, axis=1)
 
-    if "maxhr" in flist:
-        ix = flist.index("maxhr")
-        features_arr[:, ix] = np.amax(hr, axis=1)
+        if "maxhr" in flist:
+            ix = flist.index("maxhr")
+            features_arr[:, ix] = np.amax(hr, axis=1)
 
-    if "minhr" in flist:
-        ix = flist.index("minhr")
-        features_arr[:, ix] = np.amin(hr, axis=1)
+        if "minhr" in flist:
+            ix = flist.index("minhr")
+            features_arr[:, ix] = np.amin(hr, axis=1)
 
-    if "medianhr" in flist:
-        ix = flist.index("medianhr")
-        features_arr[:, ix] = np.median(hr, axis=1)
+        if "medianhr" in flist:
+            ix = flist.index("medianhr")
+            features_arr[:, ix] = np.median(hr, axis=1)
 
-    if "sdhr" in flist:
-        ix = flist.index("sdhr")
-        features_arr[:, ix] = np.std(hr, axis=1)
+        if "sdhr" in flist:
+            ix = flist.index("sdhr")
+            features_arr[:, ix] = np.std(hr, axis=1)
 
-    return features_arr
+        return features_arr
+
+    elif only_names:
+        return flist
 
 
-def get_wf_feats(sig, interval):
+def get_wf_feats(sig=None, interval=None, only_names=False):
     """Computes features for the signal waveform.
 
     The input signal is segmented into sub-signals
@@ -218,28 +224,50 @@ def get_wf_feats(sig, interval):
     features_arr : numpy.array
         A 2D numpy array with the shape (#subsegments, #features).
     """
-    b = int(len(sig) / interval)
-    subseqs = []
-    for i in range(b):
-        subseq = sig[i * interval : (i + 1) * interval]
-        subseqs.append(subseq)
-    subseqs = np.array(subseqs)  # interval=36---> (300,36)
-    f1 = np.amax(subseqs, axis=1)
-    f2 = np.amin(subseqs, axis=1)
-    f3 = np.mean(subseqs, axis=1)
-    f4 = np.std(subseqs, axis=1)
-    f5 = np.median(subseqs, axis=1)
-    f6 = stats.skew(subseqs, axis=1)
-    f7 = stats.kurtosis(subseqs, axis=1)
-    sqr = subseqs**2
-    f8 = np.amax(sqr, axis=1)
-    f9 = np.amin(sqr, axis=1)
-    f10 = np.mean(sqr, axis=1)
-    f11 = np.std(sqr, axis=1)
-    f12 = np.median(sqr, axis=1)
-    f13 = stats.skew(sqr, axis=1)
-    f14 = stats.kurtosis(sqr, axis=1)
-    features_arr = np.vstack(
-        (f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14)
-    )
-    return features_arr.T
+
+    flist = [
+        "max",
+        "min",
+        "mean",
+        "std",
+        "median",
+        "skew",
+        "kurtosis",
+        "max(squared)",
+        "min(squared)",
+        "mean(squared)",
+        "std(squared)",
+        "median(squared)",
+        "skew(squared)",
+        "kurtosis(squared)",
+    ]
+
+    if not only_names:
+        b = int(len(sig) / interval)
+        subseqs = []
+        for i in range(b):
+            subseq = sig[i * interval : (i + 1) * interval]
+            subseqs.append(subseq)
+        subseqs = np.array(subseqs)  # interval=36---> (300,36)
+        f1 = np.amax(subseqs, axis=1)
+        f2 = np.amin(subseqs, axis=1)
+        f3 = np.mean(subseqs, axis=1)
+        f4 = np.std(subseqs, axis=1)
+        f5 = np.median(subseqs, axis=1)
+        f6 = stats.skew(subseqs, axis=1)
+        f7 = stats.kurtosis(subseqs, axis=1)
+        sqr = subseqs**2
+        f8 = np.amax(sqr, axis=1)
+        f9 = np.amin(sqr, axis=1)
+        f10 = np.mean(sqr, axis=1)
+        f11 = np.std(sqr, axis=1)
+        f12 = np.median(sqr, axis=1)
+        f13 = stats.skew(sqr, axis=1)
+        f14 = stats.kurtosis(sqr, axis=1)
+        features_arr = np.vstack(
+            (f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14)
+        )
+
+        return features_arr.T
+    elif only_names:
+        return flist
